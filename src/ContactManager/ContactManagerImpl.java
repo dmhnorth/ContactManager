d0d57da86309a4a1fa9968ef46f318ddc8e8f4cb
David North <dmhnorth@gmail.com>
2014-01-20 23:00:04 +0000

    restarting contact manager now nodes have all been completed

--- src/ContactManager/ContactManagerImpl.java
+++ src/ContactManager/ContactManagerImpl.java
@@ -16,11 +16,6 @@ import java.util.Set;
  *
  */
 public class ContactManagerImpl implements ContactManager {
-
-	
-	private Set<Contact> contactsList = new HashSet();
-	private ArrayList<Meeting> meetingList = new ArrayList<Meeting>();
-	private Calendar cNow = Calendar.getInstance();
 	
 	
 	@Override
@@ -31,63 +26,34 @@ public class ContactManagerImpl implements ContactManager {
 
 	@Override
 	public PastMeeting getPastMeeting(int id) {
-		try {
-			for (Meeting m : meetingList)
-			    if (m.getId() == id)
-			return (PastMeeting) m;		
-		} catch (Exception e) {
-			return null;
-		}
+		PastMeeting meeting = null;
+		
+		return meeting;
 	}
 
 	@Override
 	public FutureMeeting getFutureMeeting(int id) {
-		try {
-			for (Meeting m : meetingList)
-			    if (m.getId() == id)
-			return (FutureMeeting) m;		
-		} catch (Exception e) {
-			return null;
-		}
+		FutureMeeting meeting = null;
+		
+		return meeting;
 	}
 
 	@Override
 	public Meeting getMeeting(int id) {
-		try {
-			for (Meeting m : meetingList)
-			    if (m.getId() == id)
-			return (MeetingImpl) m;
-		} catch (Exception e) {
-			return null;					
-		}
+		Meeting meeting = null;
+		
+		return meeting;
 	}
-
+	
 	@Override
 	public List<Meeting> getFutureMeetingList(Contact contact) {
+		List<Meeting> result = new ArrayList<Meeting>();
 		
-		List result = new ArrayList();
-		
-		for (Meeting m : meetingList)
-		    for (ContactImpl c : m.meetingContacts)//clean up this line so it searches each meeting in the meeting list.
-		    	if (c.getId() == contact.getId())
-		    		result.add(m);
-				
-				return result;
+		return result;	    	
 	}
 
 	@Override
 	public List<Meeting> getFutureMeetingList(Calendar date) {
-		
-		List result = new ArrayList();
-		
-		Iterator<MeetingImpl> it = meetingList.iterator();
-		while(it.hasNext())
-		{
-		    MeetingImpl meet = it.next();
-		    	if(meet.dateOfMeeting.equals(date)) {
-		    		result.add(meet);
-		    	}
-		}
 		
 		return null;
 	}
@@ -107,27 +73,13 @@ public class ContactManagerImpl implements ContactManager {
 	//TODO all a bit of a mess at the moment
 	@Override
 	public void addMeetingNotes(int id, String text) {
-		try {
-			for (Meeting m : meetingList)
-			    if (m.getId() == id) {
 	
-			    	if (m.notes.equals("No notes yet.")) {
-			    		m.notes = text;
-			    	} else {
-			    		m.notes += "; " + text;
-			    	}
-			    }
-		} catch (Exception e) {
-			System.out.println("You didn't add any notes.");
-		}
 	}
 		
 	
 
 	@Override
 	public void addNewContact(String name, String notes) {
-		Contact name1 = new ContactImpl(notes);
-		
 		
 	}
 
--- src/ContactManagerTests/ContactManagerTest.java
+++ src/ContactManagerTests/ContactManagerTest.java
@@ -15,13 +15,7 @@ import org.junit.Test;
  */
 public class ContactManagerTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -29,108 +23,5 @@ public class ContactManagerTest {
 	public void setUp() throws Exception {
 	}
 
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#addFutureMeeting(java.util.Set, java.util.Calendar)}.
-	 */
-	@Test
-	public void testAddFutureMeeting() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getPastMeeting(int)}.
-	 */
-	@Test
-	public void testGetPastMeeting() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getFutureMeeting(int)}.
-	 */
-	@Test
-	public void testGetFutureMeeting() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getMeeting(int)}.
-	 */
-	@Test
-	public void testGetMeeting() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getFutureMeetingList(ContactManager.Contact)}.
-	 */
-	@Test
-	public void testGetFutureMeetingListContact() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getFutureMeetingList(java.util.Calendar)}.
-	 */
-	@Test
-	public void testGetFutureMeetingListCalendar() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getPastMeetingList(ContactManager.Contact)}.
-	 */
-	@Test
-	public void testGetPastMeetingList() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#addNewPastMeeting(java.util.Set, java.util.Calendar, java.lang.String)}.
-	 */
-	@Test
-	public void testAddNewPastMeeting() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#addMeetingNotes(int, java.lang.String)}.
-	 */
-	@Test
-	public void testAddMeetingNotes() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#addNewContact(java.lang.String, java.lang.String)}.
-	 */
-	@Test
-	public void testAddNewContact() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getContacts(int[])}.
-	 */
-	@Test
-	public void testGetContactsIntArray() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#getContacts(java.lang.String)}.
-	 */
-	@Test
-	public void testGetContactsString() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link ContactManager.ContactManagerImpl#flush()}.
-	 */
-	@Test
-	public void testFlush() {
-		fail("Not yet implemented"); // TODO
-	}
-
+	
 }
